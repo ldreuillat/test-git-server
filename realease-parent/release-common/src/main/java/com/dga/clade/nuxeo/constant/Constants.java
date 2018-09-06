c134e63eb81a431d4deaca6c9dec4265fe2912cc
Laurent DREUILLAT <ldreuillat@ausy.fr>
2018-09-06 18:53:17 +0200

    Ajout de constantes.

--- realease-parent/release-common/src/main/java/com/dga/clade/nuxeo/constant/Constants.java
+++ realease-parent/release-common/src/main/java/com/dga/clade/nuxeo/constant/Constants.java
@@ -36,6 +36,7 @@ public class Constants {
     public static final String TEST1 = "test1";
     public static final String TEST2 = "test2";
     public static final String TEST3 = "test3";
+    public static final String TEST4 = "test4";
 
     private Constants() {}
 }
